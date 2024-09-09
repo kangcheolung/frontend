10f3aa2af650dc18d78487345ddf77acfd923639
owen <owenminjong@gmail.com>
2024-09-09 19:28:36 +0900

    init : postHubing

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import styles from './page.module.css';
-import {getSession, useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -15,21 +15,11 @@ export default function Home() {
     const [isLoading, setIsLoading] = useState(true);
     const { data: session } = useSession();
     const [canVote, setCanVote] = useState(true);
-    const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+    const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080';
 
     useEffect(() => {
         checkLoginStatus();
-        fetchBrands();
-        fetchGyms();
-        checkVotingEligibility();
     }, []);
-
-    useEffect(() => {
-        const filtered = gyms.filter(gym =>
-            gym.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setFilteredGyms(filtered);
-    }, [searchTerm, gyms]);
 
     const checkLoginStatus = async () => {
         try {
@@ -45,64 +35,6 @@ export default function Home() {
         }
     };
 
-    const fetchBrands = async () => {
-        try {
-            const response = await axios.get(`${serverUrl}/api/brands`);
-            setBrands(response.data);
-        } catch (error) {
-            console.error('Error fetching brands:', error);
-        }
-    };
-
-    const handleGymButtonClick = (searchTerm) => {
-        setSearchTerm(searchTerm);
-    };
-
-    const fetchGyms = async () => {
-        await axios.get(`${serverUrl}/api/gyms`, {
-            headers: {
-                'JSESSIONID': getSession('JSESSIONID')
-            }
-        })
-            .then(response => {
-                response.data.result.forEach(gym => {
-                    gym.crowdness = convertToCrowdness(gym.crowdness);
-                })
-                setGyms(response.data.result);
-            })
-    };
-
-    const checkVotingEligibility = async () => {
-        try {
-            const response = await fetch(`${serverUrl}/api/traffic/can-vote`, {
-                credentials: 'include'
-            });
-            if (response.ok) {
-                const data = await response.json();
-                setCanVote(data);
-            } else if (response.status === 401) {
-                router.push('/');
-            }
-        } catch (error) {
-            console.error('Error checking voting eligibility:', error);
-        }
-    };
-
-    const convertToCrowdness = (level) => {
-        switch(level) {
-            case 1:
-                return 'low';
-            case 2:
-                return 'medium';
-            case 3:
-                return 'high';
-            case 4:
-                return 'veryHigh';
-            default:
-                return 'none';
-        }
-
-    }
     const handleKakaoLogin = () => {
         window.location.href = `${serverUrl}/oauth2/authorization/kakao`;
     };
@@ -110,87 +42,20 @@ export default function Home() {
     const handleLogout = () => {
         setIsLoggedIn(false);
         window.location.href = `${serverUrl}/logout`;
-    }
-
-    if (isLoading) {
-        return <div style={{ textAlign: "center", marginTop: 250 }}>정보 불러오는 중...</div>;
-    }
+    };
 
     return (
-        <div className={`${isLoggedIn ? styles.container : styles.ncontainer}`}>
-            {isLoggedIn && (
-                <>
-                    <input
-                        type="text"
-                        className={styles.searchInput}
-                        placeholder="클라이밍장 검색..."
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <div className={styles.gymButtonScrollContainer}>
-                        <div className={styles.gymButtonContainer}>
-                            <button
-                                className={styles.gymButton}
-                                onClick={() => handleGymButtonClick('')}
-                            >
-                                <span>전체</span>
-                            </button>
-                            {brands.map((brand, index) => (
-                                <button
-                                    key={index}
-                                    className={styles.gymButton}
-                                    onClick={() => handleGymButtonClick(brand.searchTerm)}
-                                >
-                                    <img src={brand.imageUrl} alt={brand.name} className={styles.gymButtonIcon}/>
-                                    <span>{brand.name}</span>
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                </>
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : isLoggedIn ? (
+                <button onClick={handleLogout}>로그아웃</button>
+            ) : (
+                <button onClick={handleKakaoLogin} className={styles.kakaoLoginButton}>
+                    카카오 로그인
+                </button>
             )}
-            <div className={styles.gymListContainer}>
-                <div className={`${styles.gymList} ${!isLoggedIn ? styles.blurred : ''}`}>
-                    {filteredGyms.length > 0 ? (
-                        filteredGyms.map(gym => (
-                            <div key={gym.id} className={styles.gymItem}>
-                                <img src={gym.logo} alt={gym.name} className={styles.gymLogo}/>
-                                <div className={styles.gymInfo}>
-                                    <div className={styles.gymName}>{gym.name}</div>
-                                    <div className={styles.gymAddress}>{gym.address}</div>
-                                </div>
-                                <div className={`${styles.crowdness} ${styles[gym.crowdness]}`}>
-                                    {gym.crowdness === 'low' ? '쾌적' : gym.crowdness === 'medium' ? '보통' :
-                                        gym.crowdness === 'high' ? '혼잡' :
-                                            gym.crowdness === 'veryHigh' ? '매우혼잡' : '미정'}
-                                </div>
-                            </div>
-                        ))
-                    ) : (
-                        <div className={styles.noResults}>검색 중...</div>
-                    )}
-                </div>
-                {!isLoggedIn && (
-                    <div className={styles.loginOverlay}>
-                        <div className={styles.loginBox}>
-                            <p>클라이밍장 혼잡도를 한눈에 보고싶다면?</p>
-                            <button onClick={handleKakaoLogin} className={styles.kakaoLoginButton}>
-                                카카오 로그인
-                            </button>
-                        </div>
-                    </div>
-                )}
-            </div>
-            {isLoggedIn &&
-                <>
-                    {canVote ? (
-                        <Link href="/input" className={styles.crowdnessButton}>혼잡도 입력하기</Link>
-                    ) : (
-                        <p className={styles.cantVoteButton}>오늘은 이미 투표하셨습니다 :)</p>
-                    )}
-                    {/*<button onClick={handleLogout} className={styles.logoutButton}>로그아웃</button>*/}
-                </>
-            }
+            {/* 여기에 나머지 컴포넌트 내용을 추가하세요 */}
         </div>
     );
 }
