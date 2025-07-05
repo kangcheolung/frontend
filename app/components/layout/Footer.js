f65bb12730ddeea2c06ecc91fd6c845badaf2bb8
kangcheolung <skhu202114111@gmail.com>
2025-07-05 15:48:31 +0900

    .

--- app/components/layout/Footer.js
+++ app/components/layout/Footer.js
@@ -1,5 +1,6 @@
 export default function Footer() {
     return (
+
         <footer className="bg-indigo-900 text-indigo-100 py-8 mt-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col md:flex-row justify-between items-center">
